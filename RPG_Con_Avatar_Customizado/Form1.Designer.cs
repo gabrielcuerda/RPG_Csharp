1933bd5e8b59786069354cfbcff173a723acb20b
eloyromerogarcia14-design <eloyromerogarcia14@gmail.com>
2026-05-17 15:54:41 +0200

    Retoques

--- RPG_Con_Avatar_Customizado/Form1.Designer.cs
+++ RPG_Con_Avatar_Customizado/Form1.Designer.cs
@@ -35,7 +35,7 @@
             this.lbl_nombreUsuario.BackColor = System.Drawing.Color.Black;
             this.lbl_nombreUsuario.Font = new System.Drawing.Font("Courier New", 11F, System.Drawing.FontStyle.Bold);
             this.lbl_nombreUsuario.ForeColor = System.Drawing.Color.White;
-            this.lbl_nombreUsuario.Location = new System.Drawing.Point(180, 100);
+            this.lbl_nombreUsuario.Location = new System.Drawing.Point(502, 291);
             this.lbl_nombreUsuario.Name = "lbl_nombreUsuario";
             this.lbl_nombreUsuario.Size = new System.Drawing.Size(224, 17);
             this.lbl_nombreUsuario.TabIndex = 5;
@@ -47,7 +47,7 @@
             this.txtbox_nombreUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtbox_nombreUsuario.Font = new System.Drawing.Font("Courier New", 10F);
             this.txtbox_nombreUsuario.ForeColor = System.Drawing.Color.White;
-            this.txtbox_nombreUsuario.Location = new System.Drawing.Point(180, 125);
+            this.txtbox_nombreUsuario.Location = new System.Drawing.Point(502, 316);
             this.txtbox_nombreUsuario.Name = "txtbox_nombreUsuario";
             this.txtbox_nombreUsuario.Size = new System.Drawing.Size(250, 23);
             this.txtbox_nombreUsuario.TabIndex = 4;
@@ -61,7 +61,7 @@
             this.btnEmpezarJuego.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEmpezarJuego.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold);
             this.btnEmpezarJuego.ForeColor = System.Drawing.Color.Gold;
-            this.btnEmpezarJuego.Location = new System.Drawing.Point(230, 320);
+            this.btnEmpezarJuego.Location = new System.Drawing.Point(552, 511);
             this.btnEmpezarJuego.Name = "btnEmpezarJuego";
             this.btnEmpezarJuego.Size = new System.Drawing.Size(150, 40);
             this.btnEmpezarJuego.TabIndex = 3;
@@ -81,7 +81,7 @@
             "MAGO",
             "ARQUERO",
             "VAGABUNDO"});
-            this.comboBox_clase.Location = new System.Drawing.Point(180, 195);
+            this.comboBox_clase.Location = new System.Drawing.Point(502, 386);
             this.comboBox_clase.Name = "comboBox_clase";
             this.comboBox_clase.Size = new System.Drawing.Size(250, 24);
             this.comboBox_clase.TabIndex = 6;
@@ -92,7 +92,7 @@
             this.lbl_clase.BackColor = System.Drawing.Color.Black;
             this.lbl_clase.Font = new System.Drawing.Font("Courier New", 11F, System.Drawing.FontStyle.Bold);
             this.lbl_clase.ForeColor = System.Drawing.Color.White;
-            this.lbl_clase.Location = new System.Drawing.Point(180, 170);
+            this.lbl_clase.Location = new System.Drawing.Point(502, 361);
             this.lbl_clase.Name = "lbl_clase";
             this.lbl_clase.Size = new System.Drawing.Size(134, 17);
             this.lbl_clase.TabIndex = 7;
@@ -104,7 +104,7 @@
             this.lbl_genero.BackColor = System.Drawing.Color.Black;
             this.lbl_genero.Font = new System.Drawing.Font("Courier New", 11F, System.Drawing.FontStyle.Bold);
             this.lbl_genero.ForeColor = System.Drawing.Color.White;
-            this.lbl_genero.Location = new System.Drawing.Point(180, 240);
+            this.lbl_genero.Location = new System.Drawing.Point(502, 431);
             this.lbl_genero.Name = "lbl_genero";
             this.lbl_genero.Size = new System.Drawing.Size(161, 17);
             this.lbl_genero.TabIndex = 9;
@@ -120,7 +120,7 @@
             this.comboBox_Genero.Items.AddRange(new object[] {
             "HOMBRE",
             "MUJER"});
-            this.comboBox_Genero.Location = new System.Drawing.Point(180, 265);
+            this.comboBox_Genero.Location = new System.Drawing.Point(502, 456);
             this.comboBox_Genero.Name = "comboBox_Genero";
             this.comboBox_Genero.Size = new System.Drawing.Size(250, 24);
             this.comboBox_Genero.TabIndex = 8;
@@ -131,7 +131,7 @@
             this.lblTitulo.BackColor = System.Drawing.Color.Black;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
             this.lblTitulo.ForeColor = System.Drawing.Color.Gold;
-            this.lblTitulo.Location = new System.Drawing.Point(194, 58);
+            this.lblTitulo.Location = new System.Drawing.Point(516, 249);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(263, 24);
             this.lblTitulo.TabIndex = 10;
@@ -140,7 +140,7 @@
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = global::RPG_Con_Avatar_Customizado.Properties.Resources.icono_32x32;
-            this.pictureBox1.Location = new System.Drawing.Point(474, 49);
+            this.pictureBox1.Location = new System.Drawing.Point(796, 240);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(37, 35);
             this.pictureBox1.TabIndex = 11;
@@ -150,7 +150,7 @@
             // pictureBox2
             // 
             this.pictureBox2.BackgroundImage = global::RPG_Con_Avatar_Customizado.Properties.Resources.icono_32x32;
-            this.pictureBox2.Location = new System.Drawing.Point(130, 48);
+            this.pictureBox2.Location = new System.Drawing.Point(452, 239);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(36, 36);
             this.pictureBox2.TabIndex = 12;
@@ -162,7 +162,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(25)))), ((int)(((byte)(45)))));
             this.BackgroundImage = global::RPG_Con_Avatar_Customizado.Properties.Resources.fc9642b02ca644cff040c75cbe24698c;
-            this.ClientSize = new System.Drawing.Size(635, 407);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1240, 697);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblTitulo);
